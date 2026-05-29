6f1f52c8c12af3db609cd5cc9d57cf603639625c
Saanya Mittal <m.saanyak@gmail.com>
2026-05-29 19:32:28 +0530

    Completed calculator file

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,20 @@
-let element=document.querySelectorAll('.inner');
-let res=document.querySelector('.res');
 
-function elCLicked() {
-     console.log(`${element}`);
+let res=document.querySelector('.result');
+let display=document.querySelector('.result .display');
+
+function elClicked(input) {
+    display.innerText+=input;
+}
+
+function clearDisplay() {
+    display.innerText=" ";
+}
+function ans(){
+    try{
+        display.innerText=eval(display.innerText);
+
+    } catch(err){
+        display.innerText="ERROR";
+       
+    } 
 }
